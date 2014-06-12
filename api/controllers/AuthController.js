d60b17a3ec0881d0f58b8174c143fc4948a64c64
Will Ricketts <wricketts@procur.com>
2014-06-11 20:35:45 -0400

    Added Auth and User controller with basic actions

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,25 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	register: function(req, res){
+        res.view();
+    },
+
+    processRegistration: function(req, res){
+
+    },
+
+    login: function(req, res){
+        res.view();
+    },
+
+    processLogin: function(req, res){
+
+    }
+};
+
